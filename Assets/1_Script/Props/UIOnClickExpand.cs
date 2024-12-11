9f8bff0ffd32e9597cc266e1974cab716b4079a4
Inho <yeoinho75@gmail.com>
2024-12-11 16:46:07 +0900

    feat: polish effect more clear

--- Assets/1_Script/Props/UIOnClickExpand.cs
+++ Assets/1_Script/Props/UIOnClickExpand.cs
@@ -1,14 +1,17 @@
-using HumanFactory;
 using HumanFactory.Manager;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class UIOnClickExpand : MonoBehaviour
+public class UIOnClickExpand : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     [SerializeField] private TextMeshProUGUI stageName;
-    
+    [SerializeField] private Sprite expandedSprite;
+
+    private bool isExpanded = false;
+    private Sprite originalSprite;
     private Vector2 originSize;
     private Vector2 expandedSize;
 
@@ -16,6 +19,7 @@ public class UIOnClickExpand : MonoBehaviour
     {
         originSize = GetComponent<RectTransform>().sizeDelta;
         expandedSize = new Vector2(originSize.x, originSize.y * 2f);
+        originalSprite = GetComponent<Image>().sprite;
     }
 
     public void SetStageId(int id)
@@ -25,11 +29,48 @@ public class UIOnClickExpand : MonoBehaviour
 
     public void Expand()
     {
+        isExpanded = true;
+
         GetComponent<RectTransform>().sizeDelta = expandedSize;
+
+        GetComponent<Image>().sprite = expandedSprite;
     }
 
     public void Reduce()
     {
+        isExpanded = false;
+
         GetComponent<RectTransform>().sizeDelta = originSize;
+
+        GetComponent<Image>().sprite = originalSprite;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        foreach(Transform child in transform)
+        {
+            if (child.GetComponent<TextMeshProUGUI>() != null)
+                child.GetComponent<TextMeshProUGUI>().color = Color.black;
+        }
+
+
+        if (isExpanded)
+        {
+            GetComponent<Image>().sprite = originalSprite;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.GetComponent<TextMeshProUGUI>() != null)
+                child.GetComponent<TextMeshProUGUI>().color = Color.white;
+        }
+
+        if (isExpanded)
+        {
+            GetComponent<Image>().sprite = expandedSprite;
+        }
     }
 }
