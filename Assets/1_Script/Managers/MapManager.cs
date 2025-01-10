763c9348867dd931483dd22ecd1976c21b211bdb
Inho <yeoinho75@gmail.com>
2025-01-10 21:48:44 +0900

    fix: conflict merge'

--- Assets/1_Script/Managers/MapManager.cs
+++ Assets/1_Script/Managers/MapManager.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace HumanFactory.Manager
