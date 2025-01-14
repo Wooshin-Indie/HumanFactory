945c15964d6a57b05cb4307f69c3d6b06f2ce2db
greentea1321 <terbo1145@gmail.com>
2025-01-14 18:40:08 +0900

    feat: Added one cycling button

--- Assets/1_Script/Managers/SpeedControlManager.cs
+++ Assets/1_Script/Managers/SpeedControlManager.cs
@@ -11,6 +11,7 @@ namespace HumanFactory
         [SerializeField] private Button pauseButton; // 일시정지 버튼
         [SerializeField] private Button speedUp2Button; // 배속 버튼
         [SerializeField] private Button stopButton; // 정지 버튼
+        [SerializeField] private Button oneCycleButton; // 1사이클 실행 버튼
 
         void Start()
         {
@@ -19,37 +20,43 @@ namespace HumanFactory
             pauseButton.onClick.AddListener(PauseGame);
             speedUp2Button.onClick.AddListener(SpeedUp2Game);
             stopButton.onClick.AddListener(StopGame);
+            oneCycleButton.onClick.AddListener(OneCycleGame);
 
             // 초기 상태 설정
             //Time.timeScale = 1f; // 게임 시작 시 1배속
         }
 
-        private void PlayGame()
+        private void PlayGame() // 게임 재생
         {
+            MapManager.Instance.IsOneCycling = false;
             MapManager.Instance.AddPersonWith1x();
             GameManagerEx.Instance.SetExeType(ExecuteType.Play);
             Debug.Log("게임 재생");
         }
 
-        private void PauseGame()
+        private void PauseGame() // 게임 일시정지
         {
 			GameManagerEx.Instance.SetExeType(ExecuteType.Pause);
-			Debug.Log("게임 일시정지");
         }
 
-        private void SpeedUp2Game()
+        private void SpeedUp2Game() // 게임 2배속
         {
             MapManager.Instance.DoubleCycleTime();
-			Debug.Log("게임 배속");
         }
 
-        private void StopGame()
+        private void StopGame() // 게임 리셋
         {
             MapManager.Instance.ClearHumans();
             MapManager.Instance.IdxIn = 0;
             MapManager.Instance.IdxOut = 0;
             GameManagerEx.Instance.SetExeType(ExecuteType.None);
-            Debug.Log("게임 중지");
+        }
+
+        private void OneCycleGame() // 1사이클씩 실행
+        {
+            MapManager.Instance.IsOneCycling = true;
+            MapManager.Instance.AddPersonWithOneCycling();
+            GameManagerEx.Instance.SetExeType(ExecuteType.Play);
         }
     }
 }
