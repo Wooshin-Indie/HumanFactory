a0868c1718d9789ecf9dea5d07f17c6c1cf22b46
Inho <yeoinho75@gmail.com>
2025-02-02 23:54:51 +0900

    feat: convert map

--- Assets/1_Script/UI/MenuScene/ChallengeContentUI.cs
+++ Assets/1_Script/UI/MenuScene/ChallengeContentUI.cs
@@ -10,13 +10,22 @@ namespace HumanFactory.UI
 {
     public class ChallengeContentUI : MonoBehaviour
     {
-        [SerializeField] List<TextMeshProUGUI> itemNameTexts = new List<TextMeshProUGUI>();
-        [SerializeField] List<TextMeshProUGUI> itemScoreTexts = new List<TextMeshProUGUI>();
-		[SerializeField] List<TextMeshProUGUI> graphNameTexts = new List<TextMeshProUGUI>();
-		[SerializeField] List<Image> scoreGraphs = new List<Image>();
-        [SerializeField] List<Image> maxGraphs = new List<Image>();
 
-        public void ClearInfo()
+        [SerializeField] private List<TextMeshProUGUI> itemNameTexts = new List<TextMeshProUGUI>();
+        [SerializeField] private List<TextMeshProUGUI> itemScoreTexts = new List<TextMeshProUGUI>();
+		[SerializeField] private List<TextMeshProUGUI> graphNameTexts = new List<TextMeshProUGUI>();
+		[SerializeField] private List<Image> scoreGraphs = new List<Image>();
+        [SerializeField] private List<Image> maxGraphs = new List<Image>();
+
+        private string[] keys =
+        {
+            "Challenge_Cycle",
+            "Challenge_Structures",
+            "Challenge_Died"
+        };
+
+
+		public void ClearInfo()
         {
             gameObject.SetActive(false);
         }
@@ -31,7 +40,6 @@ namespace HumanFactory.UI
                 resultData.ButtonCount,
                 resultData.KillCount
             };
-
             for (int i = 0; i < itemScoreTexts.Count; i++)
 			{
 				itemScoreTexts[i].text = $"{counts[i]}/{info.challenges[i]}";
@@ -39,13 +47,12 @@ namespace HumanFactory.UI
 
             float maxHeight = GetComponent<RectTransform>().rect.height;
             float originWidth = maxGraphs[0].GetComponent<RectTransform>().sizeDelta.x;
-        
-            itemNameTexts[0].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Cycle");
-            itemNameTexts[1].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Structures");
-            itemNameTexts[2].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Died");
-			graphNameTexts[0].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Cycle");
-			graphNameTexts[1].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Structures");
-			graphNameTexts[2].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, "Challenge_Died");
+
+            for (int i = 0; i < itemScoreTexts.Count; i++)
+			{
+				itemNameTexts[i].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, keys[i]);
+                graphNameTexts[i].SetLocalizedString(Constants.TABLE_SUCCESSPOPUPUI, keys[i]);
+			}
 
             for (int i = 0; i < maxGraphs.Count; i++)
             {
